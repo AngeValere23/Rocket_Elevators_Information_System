673a628f22d1e5302fa8d26e5d028917f16fb9ca
frederic2ec <freddy.cho@hotmail.com>
2020-07-09 18:42:12 -0400

    Hopefully fixed

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,7 @@ module RocketElevatorsInformationSystem
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-
-    config.serve_static_assets = true
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,15 +7,17 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( css/header-1.css )
-Rails.application.config.assets.precompile += %w( css/color_scheme/red.css )
-Rails.application.config.assets.precompile += %w( css/layout.css )
-Rails.application.config.assets.precompile += %w( css/essentials.css )
-Rails.application.config.assets.precompile += %w( js/quote.js )
-Rails.application.config.assets.precompile += %w( js/scripts.js )
-Rails.application.config.assets.precompile += %w( js/newsletter.js )
-Rails.application.config.assets.precompile += %w( js/revolution_slider.js )
-Rails.application.config.assets.precompile += %w( js/contact.js )
+Rails.application.config.assets.precompile += ['.js', '.css', '*.css.erb']
+Rails.application.config.assets.precompile += %w( contact.js )
+Rails.application.config.assets.precompile += %w( essentials.css )
+Rails.application.config.assets.precompile += %w( layout.css )
+Rails.application.config.assets.precompile += %w( header-1.css )
+Rails.application.config.assets.precompile += %w( red.css )
+Rails.application.config.assets.precompile += %w( scripts.js )
+Rails.application.config.assets.precompile += %w( newsletter.js )
+Rails.application.config.assets.precompile += %w( quote.js )
+Rails.application.config.assets.precompile += %w( revolution_slider.js )
+Rails.application.config.assets.precompile += %w( layout-shop.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
