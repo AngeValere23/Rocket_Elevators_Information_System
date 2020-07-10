42b10880f8936b21be0ab2267eab7ea22b0625b5
frederic2ec <freddy.cho@hotmail.com>
2020-07-10 16:08:58 -0400

    new login and register page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    def is_employee?
+        if current_user.present? then
+            Employee.where(user_id: current_user.id).exists?
+        else
+            false
+        end
+    end
+
+    helper_method :is_employee?
 end
