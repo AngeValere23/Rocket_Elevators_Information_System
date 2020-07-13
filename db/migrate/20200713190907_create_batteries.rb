46001c9cfe2995ebb7f518c508f6f156beb60991
John Freud <it.fred@outlook.com>
2020-07-13 15:12:40 -0400

    update

--- db/migrate/20200713190907_create_batteries.rb
+++ db/migrate/20200713190907_create_batteries.rb
@@ -3,13 +3,11 @@ class CreateBatteries < ActiveRecord::Migration[5.2]
     create_table :batteries do |t|
       t.references :Building, foreign_key: true
       t.string :PropertyType
-      t.sring :Status
-      t.reference :Employee
+      t.string :Status
+      t.references :Employee, foreign_key: true
       t.datetime :DateOfCommissioning
       t.datetime :DateOfLastInspec
       t.binary :CertficatOfOperation
-      t.string :Informations
-      t.text :Notes
 
       t.timestamps
     end
